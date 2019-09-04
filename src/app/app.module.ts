3b06aac85b62221577fa9425ecbfc71dd2eeb717
Alsu Islamova <a.islamova@itransition.com>
2019-09-04 16:35:24 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CitiesComponent } from './cities/cities.component';
+import { PeriodComponent } from './period/period.component';
+import { WeatherGridComponent } from './weather-grid/weather-grid.component';
+import { SearchHistoryComponent } from './search-history/search-history.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CitiesComponent,
+    PeriodComponent,
+    WeatherGridComponent,
+    SearchHistoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
